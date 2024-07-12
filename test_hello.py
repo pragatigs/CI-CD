099004aae13c53e6c2a34481ad2befadca2da1eb
pragatigs <pragatigs02@gmail.com>
2024-07-12 23:08:57 +0530

    1

--- test_hello.py
+++ test_hello.py
@@ -9,7 +9,7 @@ class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn('Hello, World!', response.data)
+        self.assertIn(b'Hello, World!', response.data)
 
 if __name__ == '__main__':
     unittest.main()
