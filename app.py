7f9430eb44acdb2ac5a36bee9c0c3fce36ce2245
pragatigs <pragatigs02@gmail.com>
2025-01-26 19:26:51 +0530

    k8s configuration

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify
-
+import logging
 app = Flask(__name__)
 app.config['TESTING'] = True 
-
+logging.basicConfig(level=logging.INFO)
 @app.route("/")
 def hello_world():
+    logging.info("Inside k8s")
     return "Hello, World!"
 
 if __name__ == "__main__":
